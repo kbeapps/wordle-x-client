ba328a3b1de503bc001c76a31d451518262250a8
JP3tri3 <JP3tri3@gmail.com>
2022-06-09 17:21:22 -0400

    setup basic file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FriendsComponent } from './dashboard/friends/friends/friends.component';
+import { GamesComponent } from './dashboard/games/games/games.component';
+import { GroupsComponent } from './dashboard/groups/groups/groups.component';
+import { ProfileComponent } from './dashboard/profile/profile/profile.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './landing/login/login/login.component';
+import { SignupComponent } from './landing/signup/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FriendsComponent,
+    GamesComponent,
+    GroupsComponent,
+    ProfileComponent,
+    LandingComponent,
+    LoginComponent,
+    SignupComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
