d4c2bf25e3ef5a5922167ff40f9d0aa3eec46099
lw3b3r <daevahush@gmail.com>
2022-06-16 19:45:19 -0400

    Modifying friends service to utilize model additions

--- src/app/dashboard/friends/friends.service.ts
+++ src/app/dashboard/friends/friends.service.ts
@@ -1,45 +1,48 @@
-import { AuthService } from '../../shared/services/auth.service';
 import { catchError, map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpRequestService } from 'src/app/shared/services';
+import { IUser } from '../../core/models/user';
+import { HttpRequestService, UserService } from 'src/app/shared/services';
 import { Observable } from 'rxjs';
-import { User } from '../../core/models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FriendsService {
   constructor(
-    private authService: AuthService,
+    private userService: UserService,
     private http: HttpRequestService
   ) {}
 
-  private findUserId(key: string, emailOrUsername: string): Observable<string> {
-    return this.http.get('user/get', key, emailOrUsername).pipe(
-      catchError((error) => {
-        // TODO intercept errors and return only error message in error handler
-        throw new Error(error.message);
-      }),
-      map((res) => {
-        if (res) {
-          let user: User = res.data as User;
-          return user._id;
-        }
-        return '';
-      })
-    );
-  }
+  // private findUserId(key: string, emailOrUsername: string): Observable<string> {
+  //   return this.http.get('user/get', key, emailOrUsername).pipe(
+  //     catchError((error) => {
+  //       // TODO intercept errors and return only error message in error handler
+  //       throw new Error(error.message);
+  //     }),
+  //     map((res) => {
+  //       if (res) {
+  //         let user: User = res.data as User;
+  //         return user._id;
+  //       }
+  //       return '';
+  //     })
+  //   );
+  // }
 
   public sendFriendRequest(emailOrUsername: string) {
     const key = emailOrUsername.includes('@') ? 'email' : 'username';
-    const userId = this.findUserId(key, emailOrUsername);
     let message = '';
 
-    this.authService.authStore;
+    const recipientUserId = '';
+    // const recipientUserId = this.findUserId(key, emailOrUsername);
 
-    message = `${userId} sent you a friend request!`;
+    if (recipientUserId) {
+      const requestingUsername = this.userService.user.username;
+      message = `${requestingUsername} sent you a friend request!`;
 
-    return this.http.patch('notification/create', {});
+      return this.http.patch('notification/create', {});
+    }
+    return;
   }
 
   public deleteFriend(userId: string) {
