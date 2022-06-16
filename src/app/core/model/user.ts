4fd0051f2f83ece42b3486d5673b5783a7f22cf2
lw3b3r <daevahush@gmail.com>
2022-06-16 18:52:09 -0400

    -id param in User model will not always be needed

--- src/app/core/model/user.ts
+++ src/app/core/model/user.ts
@@ -1,21 +1,30 @@
 export class User {
   _id: string = '';
+  email: string = '';
   username: string = '';
+  avatar: string = '';
   friends: string[] = [];
   games: string[] = [];
   groups: string[] = [];
 
   constructor(
-    _id: string,
+    email: string,
     username: string,
+    avatar: string,
     friends: string[],
     games: string[],
-    groups: string[]
+    groups: string[],
+    _id?: string
   ) {
-    this._id = _id;
+    this.email = email;
     this.username = username;
+    this.avatar = avatar;
     this.friends = friends;
     this.games = games;
     this.groups = groups;
+
+    if (_id) {
+      this._id = _id;
+    }
   }
 }
