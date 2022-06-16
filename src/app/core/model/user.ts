b662f47cb65b85fe76f76560463d09eed0debabb
lw3b3r <daevahush@gmail.com>
2022-06-16 18:22:51 -0400

    Added constructor to User model to instantiate User objects

--- src/app/core/model/user.ts
+++ src/app/core/model/user.ts
@@ -4,4 +4,18 @@ export class User {
   friends: string[] = [];
   games: string[] = [];
   groups: string[] = [];
+
+  constructor(
+    _id: string,
+    username: string,
+    friends: string[],
+    games: string[],
+    groups: string[]
+  ) {
+    this._id = _id;
+    this.username = username;
+    this.friends = friends;
+    this.games = games;
+    this.groups = groups;
+  }
 }
