6bfac598d6d24fa220a2626d9aba8de9074e9454
JP3tri3 <JP3tri3@gmail.com>
2022-06-16 16:04:37 -0400

    added updated http request handling service

--- src/app/dashboard/games/games.service.ts
+++ src/app/dashboard/games/games.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpRequestService, IResponse } from 'src/app/shared';
+import { HttpRequestService } from 'src/app/shared';
+import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 interface IGame {
   _id?: string;
@@ -18,62 +20,84 @@ interface IGame {
 })
 export class GamesService {
   constructor(private http: HttpRequestService) {}
-  // async create(game: IGame): Promise<void | undefined> {
-  //   // const game: IGame = {
-  //   //   name: name,
-  //   //   ownerId: ownerId,
-  //   //   players: players,
-  //   //   wordHistory: wordHistory,
-  //   //   type: type,
-  //   //   winCondition: winCondition,
-  //   //   wordSize: wordSize,
-  //   // };
-  //   try {
-  //     const res: IResponse | void = await this.http.post('game/create', game);
-  //   } catch (error) {
-  //     throw new Error(error instanceof Error ? error.message : undefined);
-  //   }
-  // }
-  // async get(gameId: string): Promise<void | undefined> {
-  //   try {
-  //     const res: IResponse | void = await this.http.get(
-  //       'game/get',
-  //       '_id',
-  //       gameId
-  //     );
-  //   } catch (error) {
-  //     throw new Error(error instanceof Error ? error.message : undefined);
-  //   }
-  // }
-  // async getAll(ownerId: string): Promise<void | undefined> {
-  //   try {
-  //     const res: IResponse | void = await this.http.get(
-  //       'game/getAll',
-  //       'ownerId',
-  //       ownerId
-  //     );
-  //   } catch (error) {
-  //     throw new Error(error instanceof Error ? error.message : undefined);
-  //   }
-  // }
-  // async update(gameId: string, query: object): Promise<void | undefined> {
-  //   try {
-  //     const res: IResponse | void = await this.http.patch('game/update', {
-  //       _id: gameId,
-  //       ...query,
-  //     });
-  //   } catch (error) {
-  //     throw new Error(error instanceof Error ? error.message : undefined);
-  //   }
-  // }
-  // async delete(gameId: string): Promise<void | undefined> {
-  //   try {
-  //     const res: IResponse | void = await this.http.delete(
-  //       'game/remove',
-  //       gameId
-  //     );
-  //   } catch (error) {
-  //     throw new Error(error instanceof Error ? error.message : undefined);
-  //   }
-  // }
+
+  public create(game: IGame): Observable<boolean> {
+    return this.http.post('game/create', game).pipe(
+      catchError((error) => {
+        throw new Error(error.error.message);
+      }),
+      map((res) => {
+        if (res) {
+          // implement
+          return true;
+        }
+        return false;
+      })
+    );
+  }
+
+  public get(gameId: string): Observable<boolean> {
+    return this.http.get('game/get', '_id', gameId).pipe(
+      catchError((error) => {
+        throw new Error(error.error.message);
+      }),
+      map((res) => {
+        if (res) {
+          // implement
+          return true;
+        }
+        return false;
+      })
+    );
+  }
+
+  public getAll(ownerId: string): Observable<boolean> {
+    return this.http.get('game/getAll', 'ownerId', ownerId).pipe(
+      catchError((error) => {
+        throw new Error(error.error.message);
+      }),
+      map((res) => {
+        if (res) {
+          // implement
+          return true;
+        }
+        return false;
+      })
+    );
+  }
+
+  public update(gameId: string, query: object): Observable<boolean> {
+    return this.http
+      .patch('game/update', {
+        _id: gameId,
+        ...query,
+      })
+      .pipe(
+        catchError((error) => {
+          throw new Error(error.error.message);
+        }),
+        map((res) => {
+          if (res) {
+            // implement
+            return true;
+          }
+          return false;
+        })
+      );
+  }
+
+  public delete(gameId: string): Observable<boolean> {
+    return this.http.delete('game/remove', gameId).pipe(
+      catchError((error) => {
+        throw new Error(error.error.message);
+      }),
+      map((res) => {
+        if (res) {
+          // implement
+          return true;
+        }
+        return false;
+      })
+    );
+  }
 }
--- src/app/shared/services/http-request.service.ts
+++ src/app/shared/services/http-request.service.ts
@@ -21,53 +21,32 @@ export class HttpRequestService {
   private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) {}
 
-  // async get(
-  //   route: string,
-  //   paramKey: string,
-  //   paramValue: string
-  // ): Promise<IResponse | void> {
-  //   try {
-  //     return await firstValueFrom(
-  //       this.http.get<IResponse>(
-  //         `${this.apiUrl}${route}/${paramKey}/${paramValue}`
-  //       )
-  //     );
-  //   } catch (error) {
-  //     throw new Error(
-  //       error instanceof HttpErrorResponse ? error.error.message : undefined
-  //     );
-  //   }
-  // }
+  get(
+    route: string,
+    paramKey: string,
+    paramValue: string
+  ): Observable<IResponse> {
+    return this.http.get<any>(
+      `${this.apiUrl}${route}/${paramKey}/${paramValue}`
+    );
+  }
 
-  post(route: string, payload: object) {
+  post(route: string, payload: object): Observable<IResponse> {
     return this.http.post<any>(this.apiUrl + route, payload, httpOptions);
   }
+
+  patch(route: string, payload: object): Observable<IResponse> {
+    return this.http.patch<IResponse>(
+      this.apiUrl + route,
+      payload,
+      httpOptions
+    );
+  }
+
+  delete(route: string, params: string): Observable<IResponse> {
+    return this.http.delete<IResponse>(
+      `${this.apiUrl}${route}/${params}`,
+      httpOptions
+    );
+  }
 }
-
-// async patch(route: string, payload: object): Promise<IResponse | void> {
-//   try {
-//     return await firstValueFrom(
-//       this.http.patch<IResponse>(this.apiUrl + route, payload, httpOptions)
-//     );
-//   } catch (error) {
-//     throw new Error(
-//       error instanceof HttpErrorResponse ? error.error.message : undefined
-//     );
-//   }
-// }
-
-// async delete(route: string, params: string): Promise<IResponse | void> {
-//   try {
-//     return await firstValueFrom(
-//       this.http.delete<IResponse>(
-//         `${this.apiUrl}${route}/${params}`,
-//         httpOptions
-//       )
-//     );
-//   } catch (error) {
-//     throw new Error(
-//       error instanceof HttpErrorResponse ? error.error.message : undefined
-//     );
-//   }
-// }
-// }
