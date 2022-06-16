cda24808391a3ad34085278c0ae10d14ae629981
lw3b3r <daevahush@gmail.com>
2022-06-16 18:27:31 -0400

    Added constructor to Game model for instantiation of Game objects

--- src/app/core/model/game.ts
+++ src/app/core/model/game.ts
@@ -4,9 +4,33 @@ export class Game {
   ownerId: string = '';
   players: string[] = [];
   wordHistory: string[] = [];
-  boards?: object[] = [];
+  boards: object[] = [];
   type: string = '';
   winCondition: string = '';
   wordSize: number = 0;
-  theme?: string = '';
+  theme: string = '';
+
+  constructor(
+    _id: string,
+    name: string,
+    ownerId: string,
+    players: string[],
+    wordHistory: string[],
+    type: string,
+    winCondition: string,
+    wordSize: number,
+    boards: object[],
+    theme: string
+  ) {
+    this._id = _id;
+    this.name = name;
+    this.ownerId = ownerId;
+    this.players = players;
+    this.wordHistory = wordHistory;
+    this.boards = boards;
+    this.type = type;
+    this.winCondition = winCondition;
+    this.wordSize = wordSize;
+    this.theme = theme;
+  }
 }
