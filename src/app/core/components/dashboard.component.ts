5751db66702e45a002256c18631e59d089919e28
JP3tri3 <JP3tri3@gmail.com>
2022-06-17 11:36:52 -0400

    moved loading view to root app component

--- src/app/core/components/dashboard.component.ts
+++ src/app/core/components/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadService } from '../load.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,14 +6,7 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  appIsReady: boolean = false;
-  appReadySubscription: Subscription = new Subscription();
-
-  constructor(private loadService: LoadService) {
-    this.loadService
-      .watchAppIsReady()
-      .subscribe((readyState) => (this.appIsReady = readyState));
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
