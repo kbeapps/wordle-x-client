6b4ac4750e9f5c69d5a4e6713e46a38273a8ec7e
JP3tri3 <JP3tri3@gmail.com>
2022-06-29 22:08:23 -0400

    optimized landing reactive styling down to about 300 x 300px

--- libs/shared/components/src/lib/components.module.ts
+++ libs/shared/components/src/lib/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@client/shared/material/src';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { RequestButtonComponent } from './components/request-button/request-button.component';
@@ -9,7 +10,12 @@ import { RequestButtonComponent } from './components/request-button/request-butt
 const sharedComponents = [FormInputComponent, RequestButtonComponent];
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
   declarations: [sharedComponents],
   exports: [sharedComponents],
 })
